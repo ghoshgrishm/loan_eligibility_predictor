28d7c528f9662e20df2a9b77f0631e81c7102b35
ghoshgrishm <ghoshgrishm@gmail.com>
2025-08-01 23:20:30 +0530

    streamlit testing done

---
+++ app/app.py
@@ -0,0 +1,12 @@
+#import streamlit as st
+
+#st.title("Testing")
+#st.header("Welcome")
+#st.write("Working.....")
+
+#name = st.text_input("Enter your name:")
+#if name:
+#    st.success(f"Hello {name}, I hope you are well")
+
+#number = st.slider("Pick your age", 0, 100)
+#st.write(f"Your age is {number}")
